096a1a5a9d04f48445ec8d4d3c798be2d914f192
Scott W. Vincent <scott@swvincent.com>
2022-11-18 18:01:53 -0500

    Working auth

---
+++ GkApiKeyTest/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,39 @@
+﻿namespace GkApiKeyTest.Middleware
+{
+    /// <summary>
+    /// API Key authorization middleware
+    /// </summary>
+    /// <remarks>
+    /// Based on example from
+    /// https://www.c-sharpcorner.com/article/using-api-key-authentication-to-secure-asp-net-core-web-api/
+    /// </remarks>
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string API_KEY_NAME = "XApiKey";
+
+        public ApiKeyMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(API_KEY_NAME, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(API_KEY_NAME);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
--- GkApiKeyTest/Program.cs
+++ GkApiKeyTest/Program.cs
@@ -1,9 +1,41 @@
+using GkApiKeyTest.Middleware;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Key Auth", Version = "v1" });
+    
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "ApiKey must appear in header",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "XApiKey",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+    
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { key, new List<string>() }
+    };
+    
+    c.AddSecurityRequirement(requirement);
+});
 
 var app = builder.Build();
 
@@ -15,6 +47,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+app.UseMiddleware<ApiKeyMiddleware>();
 
 var summaries = new[]
 {
